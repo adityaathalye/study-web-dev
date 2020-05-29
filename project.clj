340482fba773ada314d492da21193ed84447590c
Aditya Athalye <aditya.athalye@gmail.com>
2020-05-30 01:22:22 +0530

    ch1: bump version to 0.1.0, removing SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject guestbook "0.1.0-SNAPSHOT"
+(defproject guestbook "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.10.0"]
