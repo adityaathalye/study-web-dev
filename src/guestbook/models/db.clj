d91a0fbcaa2d571267e3714709f5113123b05cc9
Aditya Athalye <aditya.athalye@gmail.com>
2020-05-29 21:11:48 +0530

    ch1: update model to create sqlite table for
    guestbook entries. Also gitignore sqlite db file extensions.

--- src/guestbook/models/db.clj
+++ src/guestbook/models/db.clj
@@ -8,3 +8,24 @@
   {:classname "org.sqlite.JDBC" ; class for JDBC driver
    :subprotocol "sqlite"        ; the db communication protocol
    :subname "db.sq3"})          ; name of db file used by sqlite
+
+
+(defn create-guestbook-table
+  []
+  (sql/with-connection ; ensure db connection is properly cleaned after use
+    db
+    ;; first, create a table for guestbook entries
+    (sql/create-table
+     :guestbook
+     [:id "INTEGER PRIMARY KEY AUTOINCREMENT"]
+     [:timestamp "TIMESTAMP DEFAULT CURRENT_TIMESTAMP"]
+     [:name "TEXT"]
+     [:message "TEXT"])
+    ;; then, create an index for the guestbook table
+    (sql/do-commands
+     "CREATE INDEX timestamp_index ON guestbook (timestamp)")))
+
+(comment
+  ;; evaluate ONLY IF the table does not exist
+  (create-guestbook-table)
+  )
