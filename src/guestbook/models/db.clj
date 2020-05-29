b38a2991bc70b04593c43016d8b6018df6c80071
Aditya Athalye <aditya.athalye@gmail.com>
2020-05-29 21:30:11 +0530

    ch1: update model to add/read messages to/from db

--- src/guestbook/models/db.clj
+++ src/guestbook/models/db.clj
@@ -29,3 +29,31 @@
   ;; evaluate ONLY IF the table does not exist
   (create-guestbook-table)
   )
+
+
+(defn save-message
+  [name message]
+  (sql/with-connection
+    db
+    (sql/insert-values
+     :guestbook
+     [:name :message :timestamp]
+     [name message (new java.util.Date)])))
+
+(comment
+  (save-message (str "Random Bob " (rand))
+                "hello")
+  )
+
+
+(defn read-guests
+  []
+  (sql/with-connection
+    db
+    (sql/with-query-results res
+      ["SELECT * FROM guestbook ORDER BY timestamp DESC"]
+      (doall res))))
+
+(comment
+  (read-guests)
+  )
