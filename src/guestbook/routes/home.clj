e46687fbf1b3d3a816b8ed133520b70f9c34b9e0
Aditya Athalye <aditya.athalye@gmail.com>
2020-05-29 18:00:03 +0530

    ch1: display heading, form in home route

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -3,7 +3,15 @@
             [guestbook.views.layout :as layout]))
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common
+   [:h1 "Hello World!"]
+   [:p "Welcome to my guestbook app"]
+   [:hr]
+   [:form
+    [:p "Name:"]
+    [:input]
+    [:p "Message:"]
+    [:textarea {:rows 10 :cols 40}]]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
