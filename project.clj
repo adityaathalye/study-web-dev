a2b8c318749d4abdeccb5ac065b9bee10fd890d1
Aditya Athalye <aditya.athalye@gmail.com>
2020-05-29 17:50:27 +0530

    New compojure app for Dmitri's Clj Web Dev V2 book

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject guestbook "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [hiccup "1.0.5"]
+                 [ring-server "0.5.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler guestbook.handler/app
+         :init guestbook.handler/init
+         :destroy guestbook.handler/destroy}
+  :profiles
+  {:uberjar {:aot :all}
+   :production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}}
+   :dev
+   {:dependencies [[ring/ring-mock "0.4.0"] [ring/ring-devel "1.7.1"]]}})
---
+++ src/guestbook/handler.clj
@@ -0,0 +1,23 @@
+(ns guestbook.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [guestbook.routes.home :refer [home-routes]]))
+
+(defn init []
+  (println "guestbook is starting"))
+
+(defn destroy []
+  (println "guestbook is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
