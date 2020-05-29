3a4964185fba1da3911fa56f8f5b870777903791
Aditya Athalye <aditya.athalye@gmail.com>
2020-05-29 19:46:41 +0530

    ch1: update home to show guestbook messages and
    to show an input form. The form is not wired up to POST at this time.

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,17 +1,36 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :as hf]))
 
-(defn home []
+(defn show-guests []
+  (let [dummy-messages
+        [{:message "howdy" :name "bob" :timestamp nil}
+         {:message "hello" :name "alice" :timestamp nil}]]
+    [:ul.guests
+     (map (fn [msg-record]
+            [:li
+             [:blockquote (:message msg-record)]
+             [:p "-" [:cite (:name msg-record)]]
+             [:time (:timestamp msg-record)]])
+          dummy-messages)]))
+
+
+(defn home [& [name message error]]
   (layout/common
-   [:h1 "Hello World!"]
+   [:h1 "Guestbook"]
    [:p "Welcome to my guestbook app"]
+   [:p error]
+   (show-guests) ; punch in list of existing comments
    [:hr]
-   [:form
-    [:p "Name:"]
-    [:input]
-    [:p "Message:"]
-    [:textarea {:rows 10 :cols 40}]]))
+   (hf/form-to [:post "/"]
+               [:p "Name:"]
+               (hf/text-field "name" name)
+               [:p "Message:"]
+               (hf/text-area {:rows 10 :cols 40} "message" message)
+               [:br]
+               (hf/submit-button "comment"))))
+
 
 (defroutes home-routes
   (GET "/" [] (home)))
