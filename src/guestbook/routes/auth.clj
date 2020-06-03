eedd3e4e58864e1d9eb93f9739d099565d0e9cf6
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-03 18:07:58 +0530

    ch2: add first cut of registration page+form

--- src/guestbook/routes/auth.clj
+++ src/guestbook/routes/auth.clj
@@ -2,3 +2,23 @@
   (:require [compojure.core :refer [defroutes GET POST]]
             [guestbook.views.layout :as layout]
             [hiccup.form :as hf]))
+
+
+(defn registration-page
+  []
+  (layout/common
+   (hf/form-to [:post "/register"]
+               (hf/label "id" "screen name")
+               (hf/text-field "id")
+               [:br]
+               (hf/label "pass" "password")
+               (hf/password-field "pass")
+               [:br]
+               (hf/label "pass1" "retype password")
+               (hf/password-field "pass1")
+               [:br]
+               (hf/submit-button "create account"))))
+
+(comment
+  (registration-page)
+  )
