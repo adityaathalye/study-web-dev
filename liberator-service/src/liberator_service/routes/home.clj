c4bcdcfa409744ab5266658780448256a7798499
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-04 23:51:54 +0530

    ch3: factor out 'home' defresource

--- liberator-service/src/liberator_service/routes/home.clj
+++ liberator-service/src/liberator_service/routes/home.clj
@@ -1,13 +1,28 @@
 (ns liberator-service.routes.home
-  (:require [compojure.core :refer :all]
-            [liberator.core :refer [defresource resource request-method-in]]))
+  (:require [cheshire.core :refer [generate-string]]
+            [compojure.core :refer :all]
+            [liberator.core :refer [defresource request-method-in resource]]))
+
+(def users (atom ["Firstame" "Lastname"]))
+
 
 (defn home []
   (layout/common [:h1 "Hello World!"]))
 
+
+(defresource home
+  :service-available? true
+  :handle-service-not-available
+  "service is currently unavailable..."
+
+  :method-allowed? (request-method-in :get)
+  :handle-method-not-allowed (fn [ctx]
+                               (str (get-in ctx [:request :request-method])
+                                    " is not allowed"))
+  :handle-ok "Hello World!"
+  :etag "fixed-etag"
+  :available-media-types ["text/plain"])
+
+
 (defroutes home-routes
-  (ANY "/" request
-       (resource
-        :handle-ok "Hello World!!!"
-        :etag "fixed-etag"
-        :available-media-types ["text/plain"])))
+  (ANY "/" request home))
