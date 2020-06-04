7afec2a9c05a6d5441cd4d168d50f585a7e096cb
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-04 18:56:09 +0530

    ch2: add db model for user table

--- src/guestbook/models/db.clj
+++ src/guestbook/models/db.clj
@@ -57,3 +57,33 @@
 (comment
   (read-guests)
   )
+
+
+(defn create-user-table
+  []
+  (sql/with-connection
+    db
+    (sql/create-table
+     :users
+     [:id "varchar (20) PRIMARY KEY"]
+     [:pass "varchar(100)"])))
+
+
+(defn add-user-record
+  [user]
+  (sql/with-connection db
+    (sql/insert-record :users user)))
+
+
+(defn get-user
+  [id]
+  (sql/with-connection db
+    (sql/with-query-results
+      res
+      ["select * from users where id = ?" id]
+      (first res))))
+
+(comment
+  ;; create IFF not created
+  (create-user-table)
+  )
