841b80c3069d3210d89c3a82e43cbba7760c1d15
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-11 22:34:12 +0530

    ch4: insert some user records to the users db

--- database-access/src/database_access/core.clj
+++ database-access/src/database_access/core.clj
@@ -27,3 +27,32 @@
 (comment
   (create-users-table)
   )
+
+
+(defn add-user
+  [user]
+  (sql/with-connection db
+    (sql/insert-record :users user)))
+
+(comment
+  (add-user {:id "foo" :pass "bar"})
+
+  (doseq [user-x (range 10)]
+    (add-user {:id (str "foo" user-x)
+               :pass (str "bar" user-x)}))
+  )
+
+
+(defn add-user2
+  [users]
+  (sql/with-connection db
+    (apply sql/insert-records
+           :users
+           users)))
+
+(comment
+  (add-user2
+   (map (fn [n]
+          {:id (str "foo" n)
+           :pass (str "bar" n)})
+        (range 20 30))))
