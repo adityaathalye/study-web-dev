8dd6bb1f32f33c1715f388d6638c446a85cac9db
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-13 19:10:54 +0530

    ch4: add PDF reporting capability, reading from DB

--- database-access/project.clj
+++ database-access/project.clj
@@ -5,5 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/java.jdbc "0.2.3"]
-                 [postgresql/postgresql "9.1-901.jdbc4"]]
+                 [postgresql/postgresql "9.1-901.jdbc4"]
+                 [clj-pdf "2.5.1"]]
   :repl-options {:init-ns database-access.core})
---
+++ database-access/src/database_access/reporting.clj
@@ -0,0 +1,100 @@
+(ns database-access.reporting
+  (:require [clj-pdf.core :as pdf]
+            [clojure.java.jdbc :as sql]
+            [database-access.core :as dc]))
+
+(defn create-employee-table
+  []
+  (sql/create-table
+   :employee
+   [:name "varchar(50)"]
+   [:occupation "varchar(50)"]
+   [:place "varchar(50)"]
+   [:country "varchar(50)"]))
+
+(comment
+ (sql/with-connection
+   dc/db
+   (create-employee-table)))
+
+(comment
+  (sql/with-connection
+    dc/db
+    (sql/insert-rows
+     :employee
+     ["Albert Einstein", "Engineer", "Ulm", "Germany"]
+     ["Alfred Hitchcock", "Movie Director", "London", "UK"]
+     ["Wernher Von Braun", "Rocket Scientist", "Wyrzysk", "Poland"]
+     ["Sigmund Freud", "Neurologist", "Pribor", "Czech Republic"]
+     ["Mahatma Gandhi", "Lawyer", "Gujarat", "India"]
+     ["Sachin Tendulkar", "Cricket Player", "Mumbai", "India"]
+     ["Michael Schumacher", "F1 Racer", "Cologne", "Germany"])))
+
+
+(defn read-employees
+  []
+  (sql/with-connection
+    dc/db
+    (sql/with-query-results
+      rs
+      ["select * from employee"]
+      (doall rs))))
+
+(comment
+  (read-employees)
+  )
+
+;; Try out PDF reporting
+(comment
+
+  (pdf/pdf
+   [{:header "Wow that was easy"}
+    [:list
+     [:chunk {:style :bold} "a bold item"]
+     "another item"
+     "yet another item"]
+    [:paragraph "I'm a paragraph"]]
+   "doc.pdf"))
+
+
+(def employee-template
+  (pdf/template [$name $occupation $place $country]))
+
+(comment
+  (employee-template
+   (read-employees))
+
+  (pdf/pdf
+   [{:header "Employee List"}
+    (into [:table
+           {:border true
+            :cell-border true
+            :header [{:background-color [0 204 0]}
+                      "Name" "Occupation" "Place" "Country"]}]
+          (employee-template
+           (read-employees)))]
+   "report.pdf")
+  )
+
+
+(def employee-template-paragraph
+  (pdf/template
+   [:paragraph
+    [:heading {:style {:size 15}} $name]
+    [:chunk {:style :bold} "occupation: "] $occupation "\n"
+    [:chunk {:style :bold} "place: "] $place "\n"
+    [:chunk {:style :bold} "country: "] $country
+    [:spacer]]))
+
+
+(comment
+  (pdf/pdf
+   [{}
+    [:heading {:size 10} "Employees"]
+    [:line]
+    [:spacer]
+    (employee-template-paragraph
+     (read-employees))]
+   "report.pdf")
+
+  )
