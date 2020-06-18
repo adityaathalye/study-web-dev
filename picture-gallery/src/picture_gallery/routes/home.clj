bbb3efa372a6f7ebe086cdf4a8f2ec331dad6b5f
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-19 00:29:22 +0530

    ch5: add register link to home page if user anonymous

--- picture-gallery/src/picture_gallery/routes/home.clj
+++ picture-gallery/src/picture_gallery/routes/home.clj
@@ -1,11 +1,15 @@
 (ns picture-gallery.routes.home
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [defroutes GET]]
+            [hiccup.element :as he]
             [picture-gallery.views.layout :as layout]))
 
+
 (defn home [request]
-  (layout/common [:h1 (str "Hello World! "
-                           (get-in request [:session :user-id]
-                                   "ANONYMOUS"))]))
+  (let [user-id (get-in request [:session :user-id])]
+    (layout/common [:h1 (str "Hello World!")]
+                   [:h2 (or user-id
+                            (he/link-to "/register" "register"))])))
+
 
 (defroutes home-routes
   (GET "/" request (home request)))
