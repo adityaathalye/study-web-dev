8d4c28b95881c4186914c7d66d915dc46309634e
Aditya Athalye <aditya.athalye@gmail.com>
2020-06-25 00:13:04 +0530

    ch5: pass file from params to upload route, and test
    success (file passed), and failure (null file). Update curl tests
    accordingly.

--- picture-gallery/src/picture_gallery/routes/upload.clj
+++ picture-gallery/src/picture_gallery/routes/upload.clj
@@ -1,16 +1,30 @@
 (ns picture-gallery.routes.upload
   (:require [compojure.core :refer [defroutes GET POST]]
-            [picture-gallery.views.upload :as vu]))
+            [picture-gallery.views.upload :as vu])
+  (:import [java.io File FileInputStream FileOutputStream]
+           [java.awt.image AffineTransformOp BufferedImage]
+           java.awt.RenderingHints
+           java.awt.geom.AffineTransform
+           javax.imageio.ImageIO))
+
 
 (defn handle-upload
-  [params]
-  (println params)
-  (vu/upload-page "success"))
+  "Process 'params' for file upload, which is of the form:
+  {:file
+    {:filename Clojure-icon.png,
+     :content-type image/png,
+     :tempfile #object[java.io.File 0x4d0cdaae /tmp/ring-multipart-2332893460722567376.tmp],
+     :size 2695}}"
+  [{:keys [filename] :as file}]
+  (println file)
+  (vu/upload-page
+   (if (empty? file)
+     "please select a file to upload"
+     "success")))
 
 
 (defroutes upload-routes
   (GET "/upload" [info]
        (vu/upload-page info))
-  (POST "/upload" {params :params}
-        (handle-upload params))
-  )
+  (POST "/upload" [file]
+        (handle-upload file)))
